254e4b92abed65fb84c3b7b0c69da428e879499d
kamaleshg2004 <kamaleshgauthaman@gmail.com>
2025-05-09 22:08:55 +0530

    Describe your update

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -4,9 +4,9 @@ export default function Hero() {
   return (
     <section id="hero" className="hero-section">
       <div className="hero-content">
-        <h1>Hello, I'm <span>KAMALESH G</span></h1>
+        <h1>Hello,I'm <span>KAMALESH G</span></h1>
         <h2>Fullstack Developer</h2>
-        <p>I build clean and modern web interfaces using React and CSS.</p>
+        <p>Building responsive and modern web applications using React, JavaScript, and Java for full-stack development.</p>
         <div className="hero-buttons">
         <a href="#projects" className="hero-btn">View My Work</a>
         <a href="/Kamalesh G_resume.pdf" download className="hero-btn resume-btn">Download Resume</a>
